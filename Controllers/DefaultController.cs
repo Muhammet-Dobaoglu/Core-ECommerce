0812ff64e5507eb9ca848867bd949c3c8c906dfd
Muhammet-Dobaoglu <muhammetdobaoglu@hotmail.com>
2023-03-18 21:55:40 +0300

    Add project files.

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,16 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace ECommerce.Controllers
+{
+    public class DefaultController : Controller
+    {
+        Context context = new Context();
+        public IActionResult Index()
+        {
+            var values = context.Customers.ToList();
+            return View(values);
+        }
+    }
+}
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace ECommerce.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _productService.TGetList();
+            return View(values);
+        }
+    }
+}
