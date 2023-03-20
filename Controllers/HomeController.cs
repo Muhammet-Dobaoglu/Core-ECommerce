262bf8710781aa54403de0db5e807c15fc968af6
Muhammet Dobaoğlu <106467744+Muhammet-Dobaoglu@users.noreply.github.com>
2023-03-20 13:20:48 +0300

    Areas

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -34,5 +34,7 @@ namespace ECommerce.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
         
+        
+        
     }
 }
