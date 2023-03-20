20b15460884be4797212f3a0e3398717b30002af
Muhammet Dobaoğlu <106467744+Muhammet-Dobaoglu@users.noreply.github.com>
2023-03-20 13:20:13 +0300

    Add Templates

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -33,5 +33,6 @@ namespace ECommerce.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        
     }
 }
